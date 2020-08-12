65222dbe326581574d91522f4e05fd1404cb7caf
Александр Г. Тихомиров <tikhomirov@yamoney.ru>
2020-08-12 19:07:38 +0300

    занятие 4: знакомство с DOM

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,11 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    let lengthTable = table.rows.length;
+
+    for (let i = 0; i < lengthTable; i++) {
+        table.rows[i].cells[i].style.backgroundColor = 'red';
+    };
+
+    return table;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,35 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let lengthTable = table.rows.length;
+
+    for (let i = 0; i < lengthTable; i++) {
+        let valueAvailable = '';
+        
+        if (table.rows[i].cells[3].dataset.available == 'true') {
+            valueAvailable = 'available';
+        } else if (table.rows[i].cells[3].dataset.available == 'false') {
+            valueAvailable = 'unavailable';
+        };
+        
+        if (valueAvailable != '') {
+            table.rows[i].classList.add(valueAvailable);
+        } 
+
+        if (table.rows[i].hasAttribute('data-available') == false) {
+            table.rows[i].setAttribute('hidden', true);
+        } 
+
+        if (table.rows[i].cells[2].innerHTML == 'f') {
+            table.rows[i].classList.add('female');
+        } else {
+            table.rows[i].classList.add('male');
+        }  
+
+        if (table.rows[i].cells[1].innerHTML < 18) {
+            table.rows[i].style = "text-decoration: line-through";
+        }  
+    }
+
+    return table;
 }
